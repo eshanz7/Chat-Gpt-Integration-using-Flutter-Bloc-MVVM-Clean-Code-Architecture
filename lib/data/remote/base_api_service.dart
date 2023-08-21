e1e8522cd0f99c871805beb60824b8577dbd2ee2
eshanz7 <codewithzee@gmail.com>
2023-08-21 16:56:49 +0500

    Key Removed
    Signed-off-by: eshanz7 <codewithzee@gmail.com>

--- lib/data/remote/base_api_service.dart
+++ lib/data/remote/base_api_service.dart
@@ -3,6 +3,6 @@ abstract class BaseApiService {
 
   static const String baseurl = 'https://api.openai.com/';
 
-  static const String apiKey = "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";
+  static const String apiKey = "";
 
  }
